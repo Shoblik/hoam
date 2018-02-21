2753e0431b1968bea3fa5d7bbe1f95153e50b35a
Shoblik <shoblik@yahoo.com>
2018-02-20 17:27:31 -0800

    completed the user authentication endpoint when signing up for the app

---
+++ actions/read_authenticate.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shobl
+ * Date: 2/20/2018
+ * Time: 4:49 PM
+ */
+
+if(!isset($PAGEACCESS) || $PAGEACCESS===false){
+    die('NO DIRECT ACCESS ALLOWED');
+}
+$output['success'] = true;
+$output['newUser'] = true;
+$phoneNumber = $_POST['phoneNumber'];
+$phoneNumber = '001' . $phoneNumber;
+
+//query the database checking if the users phone number already exists
+$query = "SELECT `phone`
+          FROM `users`
+          WHERE `phone` = $phoneNumber";
+$result = mysqli_query($conn, $query);
+
+
+if ($result) {
+    if (mysqli_num_rows($result) > 0) {
+        $output['newUser'] = false;
+    }
+} else {
+    $output['errors'][] = 'Error in query';
+}
+?>
