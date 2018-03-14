7a3fdd549e1688d86a95477b9ccc4247fde0e53a
Shoblik <shoblik@yahoo.com>
2018-03-14 13:41:53 -0700

    finished the create user function

--- actions/create_user.php
+++ actions/create_user.php
@@ -14,6 +14,7 @@ $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 $name = $_GET['nickName'];
 $pin = $_GET['pin'];
+$formattedGPS = $lat . ', ' . $lng;
 $zipCode = null;
 $formattedAddress = null;
 
@@ -32,25 +33,83 @@ function getAddress($lat, $lng) {
         getAddress($lat, $lng);
     }
 };
-getAddress($lat, $lng);
+    getAddress($lat, $lng);
 
-$output['zipCode'] = $zipCode;
-$output['address'] = $formattedAddress;
+
+    //generate HOA list
+    function getHoaList($zipCode, &$output) {
+        $urlArr = ['https://www.allpropertymanagement.com/find/index.php?thisSearchPage=HOME&search=Y&t=71&zip=' . $zipCode . '&submit=', 'https://www.allpropertymanagement.com/find/index.php?thisSearchPage=HOME&search=Y&t=73&zip=' . $zipCode . '&submit=', 'https://www.allpropertymanagement.com/find/index.php?thisSearchPage=HOME&search=Y&t=76&zip=' . $zipCode . '&submit='];
+        $count = count($urlArr);
+        $bizArray = [];
+        $tempBizName = '';
+        $findGreaterThan = false;
+        $scraping = false;
+
+        for ($urlIndex = 0; $urlIndex < $count; $urlIndex++) {
+            $url = $urlArr[$urlIndex];
+
+            $content = @file_get_contents($url);
+            $length = strlen($content);
+
+            for ($i=0; $i<$length; $i++) {
+                if ($content[$i] === 'b' && $content[$i + 1] === 'i' && $content[$i + 2] === 'z' && $content[$i + 3] === '_' && $content[$i + 4] === 'n' && $content[$i + 5] === 'a' && $content[$i + 6] === 'm' && $content[$i + 7] === 'e') {
+                    $findGreaterThan = true;
+                }
+                else if ($content[$i] === '>' && $findGreaterThan === true) {
+                    $scraping = true;
+                }
+                else if ($content[$i] === '<' && $findGreaterThan === true) {
+                    $findGreaterThan = false;
+                    $scraping = false;
+                    $bizArray[] = $tempBizName;
+                    $tempBizName = null;
+                }
+                else if ($scraping) {
+                    $tempBizName = $tempBizName . $content[$i];
+
+                }
+            }
+
+        }
+        $bizArrCount = count($bizArray);
+        $cleanedBizArray = array();
+
+        for ($i=0; $i<$bizArrCount; $i++) {
+            $cleanedBizArray[$bizArray[$i]] = 1;
+        }
+        $output['data'] = array_keys($cleanedBizArray);
+    }
+
+    getHoaList($zipCode, $output);
+
 
 $query = "SELECT * FROM `auth` WHERE `phone` = '$phoneNumber' AND `pin` = $pin";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
-    $output['count'] = mysqli_num_rows($result);
     if (mysqli_num_rows($result) > 0) {
         $output['validCreds'] = true;
     } else {
         $output['validCreds'] = false;
+        $output['createdUser'] = false;
     }
 } else {
-    $output['errors'][] = 'Error in query';
+    $output['errors'][] = 'Error in query finding authorization credentials';
 }
 
 if ($output['validCreds']) {
+    //create new user in database
+    $query = "INSERT INTO `users` (`name`, `phone`, `gps_loc`, `address`, `created`, `updated`, `active`) 
+              VALUES ('$name', '$phoneNumber', '$formattedGPS', '$formattedAddress', CURRENT_DATE, CURRENT_DATE, 'active')";
+    $result = mysqli_query($conn, $query);
 
+    if ($result) {
+        if (mysqli_affected_rows($conn) > 0) {
+            $output['createdUser'] = true;
+        } else {
+            $output['createdUser'] = false;
+        }
+    } else {
+        $output['errors'][] = 'Error inserting new user';
+    }
 }
